d48fc2b082156b954614702e43e98b6ec3b9045f
danielsoftdeleloper <daniel.andrade@sga.pucminas.br>
2021-01-29 17:00:41 -0300

    Adicionado as operações de cálculo de porcentagem.

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,23 +1,24 @@
 onload = () => {
-  document.querySelector('#bt-0').onclick = () => digito(0);
-  document.querySelector('#bt-1').onclick = () => digito(1);
-  document.querySelector('#bt-2').onclick = () => digito(2);
-  document.querySelector('#bt-3').onclick = () => digito(3);
-  document.querySelector('#bt-4').onclick = () => digito(4);
-  document.querySelector('#bt-5').onclick = () => digito(5);
-  document.querySelector('#bt-6').onclick = () => digito(6);
-  document.querySelector('#bt-7').onclick = () => digito(7);
-  document.querySelector('#bt-8').onclick = () => digito(8);
-  document.querySelector('#bt-9').onclick = () => digito(9);
-  document.querySelector('#bt-comma').onclick = virgula;
-  document.querySelector('#bt-00').onclick = zeroZero;
-  document.querySelector('#bt-ac').onclick = limpa;
-  document.querySelector('#bt-divide').onclick = () => operador("/");
-  document.querySelector('#bt-times').onclick = () => operador("*");
-  document.querySelector('#bt-minus').onclick = () => operador("-");
-  document.querySelector('#bt-plus').onclick = () => operador("+");
-  document.querySelector('#bt-equals').onclick = () => calcula();
-  
+  document.querySelector("#bt-0").onclick = () => digito(0);
+  document.querySelector("#bt-1").onclick = () => digito(1);
+  document.querySelector("#bt-2").onclick = () => digito(2);
+  document.querySelector("#bt-3").onclick = () => digito(3);
+  document.querySelector("#bt-4").onclick = () => digito(4);
+  document.querySelector("#bt-5").onclick = () => digito(5);
+  document.querySelector("#bt-6").onclick = () => digito(6);
+  document.querySelector("#bt-7").onclick = () => digito(7);
+  document.querySelector("#bt-8").onclick = () => digito(8);
+  document.querySelector("#bt-9").onclick = () => digito(9);
+  document.querySelector("#bt-comma").onclick = virgula;
+  document.querySelector("#bt-00").onclick = zeroZero;
+  document.querySelector("#bt-ac").onclick = limpa;
+  document.querySelector("#bt-divide").onclick = () => operador("/");
+  document.querySelector("#bt-times").onclick = () => operador("*");
+  document.querySelector("#bt-minus").onclick = () => operador("-");
+  document.querySelector("#bt-plus").onclick = () => operador("+");
+  document.querySelector("#bt-equals").onclick = () => calcula();
+  document.querySelector("#bt-per").onclick = () => porcento();
+  document.querySelector("#bt-backspace").onclick = () => apagarvalor();
 };
 
 let sValor = "0"; /* valor que sera apresentado no display */
@@ -25,6 +26,7 @@ let ehNovoNumero = true; /* Indica se o próximo digito sera um novo número */
 let valorAnterior = null; /* Valor acumulado para o calculo */
 let operacaoPendente = null; /* operação acumulada */
 let ValorX = null;
+let val = false;
 
 /* atualização do visor */
 const atualizaVisor = () => {
@@ -34,6 +36,8 @@ const atualizaVisor = () => {
   /* desistruturação de vetor */
   let [parteInteira, parteDecimal] = sValor.split(",");
   let v = "";
+  /*  console.log(parte[0]);
+  console.log(parte[1]); */
   c = 0;
   for (let i = parteInteira.length - 1; i >= 0; i--) {
     if (++c > 3) {
@@ -44,6 +48,7 @@ const atualizaVisor = () => {
   }
   v = v + (parteDecimal ? "," + parteDecimal : "");
   document.querySelector("#display").innerText = v;
+  /* console.log("display", v); */
 };
 
 /* tratamento do clique no botao digito ok*/
@@ -79,40 +84,102 @@ const zeroZero = () => {
 const limpa = () => {
   ehNovoNumero = true;
   valorAnterior = 0;
-  sValor = '0';
+  sValor = "0";
   operacaoPendente = null;
+  val = false;
   atualizaVisor();
 };
 
 /* Funções em construção */
 
 /* Converte a string do valor para um número real */
-const valorAtual = () => parseFloat(sValor.replace(',', '.'));
+const valorAtual = () => parseFloat(sValor.replace(",", "."));
 
 /* tratamento do clique nos botões operadores */
 const operador = (op) => {
   calcula();
   valorAnterior = valorAtual();
   operacaoPendente = op;
-  ehNovoNumero = true; /* proxio representa novo número */
-  
+  ehNovoNumero = true; /* próximo representa novo número */
 };
 
 const calcula = () => {
-    if(operacaoPendente != null){
-        let resultado;
-        switch(operacaoPendente){
-            case '+' : resultado = valorAnterior + valorAtual(); break;
-            case '-' : resultado = valorAnterior - valorAtual(); break;
-            case '*' : resultado = valorAnterior * valorAtual(); break;
-            case '/' : resultado = valorAnterior / valorAtual(); break;
-            
-        }
-        sValor = resultado.toString().replace('.', ',');
+  if (operacaoPendente != null) {
+    let resultado;
+    let opcao = null;
 
+    switch (operacaoPendente) {
+      case "+":
+        resultado = valorAnterior + valorAtual();
+        opcao = 1;
+        break;
+      case "-":
+        resultado = valorAnterior - valorAtual();
+        opcao = 2;
+        break;
+      case "*":
+        resultado = valorAnterior * valorAtual();
+        opcao = 3;
+        break;
+      case "/":
+        resultado = valorAnterior / valorAtual();
+        opcao = 4;
+        break;
     }
-    ehNovoNumero = true;
-    operacaoPendente = null;
-    valorAnterior = 0;
-    atualizaVisor();
-}
+    if (val) {
+      if (opcao == 1) {
+        let valorTemp = null;
+        /*  console.log("resultadoOperacao1", resultado); */
+        /*   console.log("Temp1", valorTemp);
+        console.log(valorAtual); */
+        valorTemp = (valorAnterior / 100) * valorAtual();
+        /* console.log("temp2", valorTemp); */
+        /* console.log("resultado", resultado); */
+        resultado = valorAnterior + valorTemp;
+      }
+      if (opcao == 2) {
+        let valorTemp = null;
+        /*  console.log("resultadoOperacao1", resultado); */
+        /*   console.log("Temp1", valorTemp);
+        console.log(valorAtual); */
+        valorTemp = (valorAnterior / 100) * valorAtual();
+        /* console.log("temp2", valorTemp); */
+        /* console.log("resultado", resultado); */
+        resultado = valorAnterior - valorTemp;
+      }
+      if (opcao == 3) {
+        /* console.log("resultadoOperacao1", resultado); */
+        resultado = (valorAnterior / 100) * valorAtual();
+        /*  console.log("resultadodaOperacao3", resultado); */
+        /* console.log("resultadoFinalNaVariavel3", sValor); */
+      }
+      if (opcao == 4) {
+       /*  console.log("resultadoOperacao1", resultado); */
+        resultado = (valorAnterior * 100) / valorAtual();
+        /* console.log("resultadodaOperacao3", resultado);
+        console.log("resultadoFinalNaVariavel3", sValor); */
+      }
+      /* console.log("resultadoForaIf", resultado); */
+      resultado = resultado;
+      /*  console.log("resultadoFinalForaIF2", sValor); */
+      val = false;
+    }
+
+    sValor = resultado.toString().replace(".", ",");
+  }
+
+  ehNovoNumero = true;
+  operacaoPendente = null;
+  valorAnterior = 0;
+  atualizaVisor();
+};
+
+const apagarvalor = () => {
+  
+};
+
+const porcento = () => {
+  val = true;
+  calcula();
+  /* console.log("val", val); */
+};
