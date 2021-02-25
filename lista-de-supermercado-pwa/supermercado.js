b42136ee7e3c8600a79689eba4d7656a4b0930e3
danielsoftdeleloper <daniel.andrade@sga.pucminas.br>
2021-02-25 19:04:02 -0300

    Elemento de navegação formulário

--- lista-de-supermercado-pwa/supermercado.js
+++ lista-de-supermercado-pwa/supermercado.js
@@ -1,30 +1,37 @@
-let telas = ["componente1, componente2"];
+let telas = ['componente1, componente2, componente3'];
+
 const mostra = (comp) => {
   telas.forEach((c) => {
     /* esconde os dois componentes */
-    document.querySelector("#" + c).classList.add("hidden");
+    console.log('entrou');
+    document.querySelector('#' + c).classList.add('hidden');
   });
-
-  document.querySelector("#" + comp).classList.remove("hidden");
+  document.querySelector('#' + comp).classList.remove('hidden');
 };
 
 const ativa = (elem) => {
   /* recebe todos elementos dos parentes clicados e dele próprio */
   let irmaos = elem.parentNode.children;
-  /* for padrão por não ser um vetor simples */
-  for (let i = 0; i < irmaos.length; i++)
+  for (let i = 0; i < irmaos.length; i++) {
+    /* for padrão por não ser um vetor simples */
     irmaos[i].classList.remove('active'); /* remmove do irmaos */
-  elem.classList.add('active'); /* activa no elemento clicado */
+    elem.classList.add('active'); /* activa no elemento clicado */
+  }
 };
 
 onload = () => {
-  document.querySelector("#tab1").onclick = (e) => {
-    mostra("componente1");
+  document.querySelector('#tab1').onclick = (e) => {
+    mostra('componente1');
     ativa(e.target);
   };
 
-  document.querySelector("#tab2").onclick = (e) => {
-    mostra("componente2");
+  document.querySelector('#tab2').onclick = (e) => {
+    mostra('componente2');
+    ativa(e.target);
+  };
+
+  document.querySelector('#tab3').onclick = (e) => {
+    mostra('componente3');
     ativa(e.target);
   };
 };
