0702d1a9cb442dedf1702f27f8ea67bdf5b589c5
danielsoftdeleloper <daniel.andrade@sga.pucminas.br>
2021-03-20 03:07:54 -0300

    Lista de Prodtos e contador badge

--- lista-de-supermercado-pwa/supermercado.js
+++ lista-de-supermercado-pwa/supermercado.js
@@ -1,11 +1,15 @@
-let telas = ['componente1, componente2, componente3, componente4'];
+let telas = ['componente1, componente2'];
+let conta = 0;
+
+/* console.log('vetor principal', telas); */
 
 const mostra = (comp) => {
-  let i = false;
   telas.forEach((c) => {
     //esconde os dois componentes
-
+    /*  console.log(c); */
     document.querySelector('#' + c).classList.add('hidden');
+    /* 
+    console.log(comp); */
   });
 
   document.querySelector('#' + comp).classList.remove('hidden');
@@ -21,11 +25,7 @@ const ativa = (elem) => {
   }
 };
 
-
-
 onload = () => {
-
-  
   document.querySelector('#tab1').onclick = (e) => {
     mostra('componente1');
     ativa(e.target);
@@ -35,64 +35,60 @@ onload = () => {
     mostra('componente2');
     ativa(e.target);
   };
-
-  document.querySelector('#tab3').onclick = (e) => {
-    mostra('componente3');
-    ativa(e.target);
-  };
-
-  document.querySelector('#tab4').onclick = (e) => {
-    mostra('componente4');
-    ativa(e.target);
-  };
-
 };
-filterSelection("all")
+
+filterSelection('all');
 function filterSelection(c) {
   var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
+
+  x = document.getElementsByClassName('filterDiv');
+  if (c == 'all') c = '';
   // Adicione a classe "show" (display: block) aos elementos filtrados e remova a classe "show" dos elementos que não estão selecionados
 
-
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    w3RemoveClass(x[i], 'show');
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], 'show');
   }
 }
 
 // Mostrar filtros de elementos
 function w3AddClass(element, name) {
+  conta++;
+
+  /* document.querySelector('.badge').innerText = name.length; */
+
   var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
+  arr1 = element.className.split(' ');
+  arr2 = name.split(' ');
+
   for (i = 0; i < arr2.length; i++) {
     if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+      element.className += ' ' + arr2[i];
     }
   }
+  document.querySelector('.badge').innerText = conta;
 }
 
 //  Esconder elementos fora da categoria selecionada.
 function w3RemoveClass(element, name) {
   var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
+  arr1 = element.className.split(' ');
+  arr2 = name.split(' ');
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
       arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
   }
-  element.className = arr1.join(" ");
+  element.className = arr1.join(' ');
 }
 
 // Adicionar classe ativa ao botão de controle atual (destaque)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btnContainer = document.getElementById('myBtnContainer');
+var btns = btnContainer.getElementsByClassName('btn');
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+  btns[i].addEventListener('click', function () {
+    var current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace(' active', '');
+    this.className += ' active';
   });
 }
